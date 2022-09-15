1a3f38132eb33453b1334d8241c9788f2737fcff
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2022-09-15 11:19:48 -0700

    Initial commit

--- cpp-switch-statement/Main.cpp
+++ cpp-switch-statement/Main.cpp
@@ -6,7 +6,32 @@ using namespace std;
 
 int main() {
 
-    cout << "Using Switch Statement";
+    string city = "Yola";
+    cout << "Enter the name of your city: ";
+    string choice;
+    
+    switch(city)
+    {
+        case "Abuja":
+            choice = "You are in Abuja";
+            break;
+        case "Lagos":
+            choice = "You are in Lagos";
+            break;
+        case "Kano":
+            choice = "You are in Kano";
+            break;
+        case "Abia":
+            choice = "You are in Abia";
+            break;
+        case "PortHarCourt":
+            choice = "You are in PortHarCourt";
+            break;
+        default:
+            choice = "You are not in one of the mentioning cities";
+            break;
+    }
+    cout << city << NEWLINE;
 
     return 0;
 }
