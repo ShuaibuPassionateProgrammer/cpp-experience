b09fd0e3a2aeb018168d4fb13abf065dc5f06f89
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2022-09-17 06:36:00 -0700

    Initial commit

--- cpp-pseudo-random-number/Main.cpp
+++ cpp-pseudo-random-number/Main.cpp
@@ -5,6 +5,15 @@ using namespace std;
 int main() {
 
     cout << "Pseudo Random Number Generator" << endl;
+    cout << "______________________________" << endl;
+
+    srand(time(NULL));
+    
+    int number1 = rand() % 10 + 1;
+    int number2 = rand() % 20 + 1;
+    int number3 = rand() % 30 + 1;
+    
+    cout << number1 << "\t" << number2 << "\t" << number3 << "\n";
 
     return 0;
 }
