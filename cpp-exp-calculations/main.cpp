d452f98d7ca0df623906abea87e5444dd25d7e8d
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-02-16 09:50:04 -0800

    Initial commit

---
+++ cpp-exp-calculations/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    cout << "Hello world!" << endl;
+    cout << "1 2 3 4 5 6 7 8 9 0" << endl;
+    cout << 17.68 << endl;
+    cout << 838 << endl;
+    cout << "abcd" << endl;
+    cout << 'T' << endl;
+
+    cout << "C++ is amazing \t\t\t\t\t";
+    cout << "Hello Cpp" << endl;
+
+    int number = 10;
+    cout << number << endl;
+
+    int sum = 25 + 25;
+    cout << sum << endl;
+
+    int var1 = 123;
+    int var2 = 213;
+    int add = var1 + var2;
+    cout << add << endl;
+
+    float decimalValue = 83.32;
+    cout << decimalValue << endl;
+
+    return 0;
+}
