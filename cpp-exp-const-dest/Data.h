8a154faea2efecd9436282127e697c5da8b35718
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-02-17 11:01:33 -0800

    Initial commit

---
+++ cpp-exp-const-dest/Data.h
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <string>
+#define NEWLINE  '\n'
+
+using namespace std;
---
+++ cpp-exp-const-dest/Main.cpp
@@ -0,0 +1,24 @@
+#include "Data.h";
+
+class Main 
+{
+	public: Main()
+	{
+		cout << "Constructor called" << NEWLINE;
+	}
+
+	public: ~Main()
+	{
+		cout << "Destructor called" << NEWLINE;
+	}
+};
+
+Main::Main()
+{
+	// Awesome constructor
+}
+
+Main::~Main()
+{
+	// Awesome destructor
+}
