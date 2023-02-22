27c4885e671bf00417f6c6a6f5fbe12b67d07bbc
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-02-22 09:58:40 -0800

    Initial commit

--- cpp-exp-multithreading-002/Main.cpp
+++ cpp-exp-multithreading-002/Main.cpp
@@ -11,8 +11,9 @@ int main()
     std::cout << "Hello waiter\n" << std::flush;
     auto start = std::chrono::high_resolution_clock::now();
     std::this_thread::sleep_for(2000ms);
-
-
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> elapsed = end-start;
+    std::cout << "Waited " << elapsed.count() << " ms\n";
 
     return 0;
 }
