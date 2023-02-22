c5462f1722ba6725e35d972f5d0acddbf536ef15
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-02-22 15:23:21 -0800

    Initial commit

--- cpp-exp-multithreading-003/Main.cpp
+++ cpp-exp-multithreading-003/Main.cpp
@@ -5,6 +5,20 @@ using namespace std;
 int main()
 {
     std::cout << "Multi Threading exercise" << std::end;
+    cout << "\t\t\t C++ " << endl;
+	sleep(1);
+	cout << "\t\tProgramming " << endl;
+	sleep(1);
+	cout << "\t\t\t Language " << endl;
+	sleep(1);
+	cout << "\t\tis " << endl;
+	sleep(1);
+	cout << "\t\t\t Awesome " << endl;
+	sleep(1);
+	cout << "\t\tand " << endl;
+	sleep(1);
+	cout << "\t\t\t Hot " << endl;
+
 
     return 0;
 }
