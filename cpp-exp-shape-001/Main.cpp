56f4c1a757f12a2f1fab41777b85cbab2da325f8
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2023-02-23 15:53:14 -0800

    Initial commit

---
+++ cpp-exp-shape-001/Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	
+	cout << "********************" << endl;
+	cout << "*   \t       \t   * " << endl;
+	cout << "*   \t       \t   * " << endl;
+	cout << "*   \t       \t   * " << endl;
+	cout << "*   \t       \t   * " << endl;
+	cout << "*   \t       \t   * " << endl;
+	cout << "*   \t       \t   * " << endl;	
+	cout << "********************" << endl;
+	
+	return 0;
+}
